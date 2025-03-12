6a494e470c2bd94f0887f9c86b333f34c9b83a6b
ABHAY <369abhayg@gmail.com>
2025-03-12 08:46:41 +0530

    upload image and video on cloudinary

--- client/src/pages/signupPage/SignupPage.jsx
+++ client/src/pages/signupPage/SignupPage.jsx
@@ -2,10 +2,15 @@ import React, { useState } from 'react'
 import './SignupPage.css';
 import YouTubeIcon from '@mui/icons-material/YouTube';
 import { Link} from 'react-router-dom';
+import axios from 'axios';
+import Box from '@mui/material/Box';
+import LinearProgress from '@mui/material/LinearProgress';
 
 const SignupPage = () => {
 
-    const [singUpFiled, setSignUpField] = useState({ "channelName": "", "userName": "", "password": "", "about": "", "profilePicture": "" });
+    const [uploadedImageUrl, setUploadedImageUrl] = useState("https://th.bing.com/th/id/OIP.Wy2uo_y-ttULYs4chLmqSAAAAA?rs=1&pid=ImgDetMain");
+    const [singUpFiled, setSignUpField] = useState({ "channelName": "", "userName": "", "password": "", "about": "", "profilePic": uploadedImageUrl });
+    const [progressBar,setProgressBar] = useState(false);
 
 
     const handleInputFiled = (event, name) => {
@@ -14,6 +19,28 @@ const SignupPage = () => {
         })
     }
     console.log(singUpFiled)
+
+
+    const uploadImage = async (e) => {
+        console.log("Uploading")
+        const files = e.target.files;
+        const data = new FormData();
+        data.append('file', files[0]);
+        // youtube-clone
+        data.append('upload_preset', 'youtube_clone');
+        try {
+            setProgressBar(true)
+            const response = await axios.post("https://api.cloudinary.com/v1_1/dwvjikoay/image/upload", data)
+            setProgressBar(false)
+            const imageUrl = response.data.url;
+            setUploadedImageUrl(imageUrl);
+            setSignUpField({
+                ...singUpFiled, "profilePic": imageUrl
+            })
+        } catch (err) {
+            console.log(err)
+        }
+    }
 
     return (
         <div className='signUp'>
@@ -27,10 +54,11 @@ const SignupPage = () => {
                     <input type="text" value={singUpFiled.channelName} placeholder="Name" className="signUp_Inputs_inp" onChange={(e)=>handleInputFiled(e, "channelName")} />
                     <input type="text" value={singUpFiled.userName} placeholder="User Name" className="signUp_Inputs_inp" onChange={(e)=>handleInputFiled(e,"userName")}/>
                     <input type="password" value={singUpFiled.password} placeholder="Password" className="signUp_Inputs_inp" onChange={(e)=>handleInputFiled(e,"password")}/>
-                    <input type="text" value={singUpFiled.about} placeholder="About Your Channel" className="signUp_Inputs_inp" />
+                    <input type="text" value={singUpFiled.about} placeholder="About Your Channel" className="signUp_Inputs_inp" onChange={(e)=>handleInputFiled(e,"about")} />
                     <div className="image_upload_signup">
-                        <input type='file'/>
+                        <input type='file' onChange={(e)=>uploadImage(e)}/>
                         <div className='image_upload_signup_div'>
+                        <img className='image_default_signUp' src={uploadedImageUrl} />
                         </div>
                     </div>
 
@@ -39,6 +67,10 @@ const SignupPage = () => {
                         <Link to={'/'} className="signUpBtn">Home Page</Link>
 
                     </div>
+
+                    {progressBar && <Box sx={{ width: '100%' }}>
+                        <LinearProgress />
+                    </Box>}
 
                 </div>
 
--- client/src/pages/uploadPage/UploadPage.jsx
+++ client/src/pages/uploadPage/UploadPage.jsx
@@ -2,16 +2,46 @@ import React, { useState } from 'react'
 import './UploadPage.css';
 import YouTubeIcon from '@mui/icons-material/YouTube';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
 
 const UploadPage = () => {
 
-  const [inputField, setInputField] = useState({ "title": "", "description": "", "category": "", "thumbnail": "", "videoType": "" })
+  const [inputField, setInputField] = useState({ "title": "", "description": "", "videoLink": "", "thumbnail": "", "category": "" });
+  const [loader ,setLoader] = useState(false);
 
   const handleOnChangeInput = (event, name) => {
     setInputField({
-        ...inputField, [name]: event.target.value
+      ...inputField, [name]: event.target.value
     })
-}
+  }
+
+  const uploadImage = async (e, type) => {
+
+    setLoader(true)
+    console.log("Uploading")
+    const files = e.target.files;
+    const data = new FormData();
+    data.append('file', files[0]);
+    data.append('upload_preset', 'youtube_clone');
+    try {
+      const response = await axios.post(`https://api.cloudinary.com/v1_1/dwvjikoay/${type}/upload`, data)
+      const url = response.data.url;
+      setLoader(false)
+      let val = type === "image" ? "thumbnail" : "videoLink"
+      setInputField({
+        ...inputField, [val]: url
+      })
+
+    } catch (err) {
+      setLoader(false)
+      console.log(err)
+    }
+
+  }
+  console.log(inputField)
+
 
 
   return (
@@ -22,13 +52,19 @@ const UploadPage = () => {
           Upload Video
         </div>
         <div className="uploadForm">
-          <input type="text" value={inputField.title} placeholder='Title' className="uploadFormInputs" onChange={(e)=>handleOnChangeInput(e,"title")}/>
-          <input type="text" value={inputField.description} placeholder='Description' className="uploadFormInputs" onChange={(e)=>handleOnChangeInput(e,"description")}/>
-          <input type="text" value={inputField.category} placeholder='Category' className="uploadFormInputs" onChange={(e)=>handleOnChangeInput(e,"category")}/>
-          <div>Thumbnail <input type="file" accept='image/*' /></div>
-          <div>Video <input type="file" accept='video/mp4, video/webm, video/*' /></div>
+          <input type="text" value={inputField.title} placeholder='Title' className="uploadFormInputs" onChange={(e) => handleOnChangeInput(e, "title")} />
+          <input type="text" value={inputField.description} placeholder='Description' className="uploadFormInputs" onChange={(e) => handleOnChangeInput(e, "description")} />
+          <input type="text" value={inputField.category} placeholder='Category' className="uploadFormInputs" onChange={(e) => handleOnChangeInput(e, "category")} />
+          <div>Thumbnail <input type="file" accept='image/*' onChange={(e) => uploadImage(e, "image")} /></div>
+          <div>Video <input type="file" accept='video/mp4, video/webm, video/*' onChange={(e) => uploadImage(e, "video")} /></div>
 
+          {
+            loader && <Box sx={{ display: 'flex' }}>
+              <CircularProgress />
+            </Box>
+          }
         </div>
+
 
 
         <div className="uploadBtns">
