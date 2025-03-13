cd6d4365dce4c2348b8ec056950a89127add204b
ABHAY <369abhayg@gmail.com>
2025-03-13 23:52:00 +0530

    dome

--- server/index.js
+++ server/index.js
@@ -21,22 +21,6 @@ app.use(cors({
 }))
 
 
-
-// app.get('/api/search', async (req, res) => {
-//   const searchQuery = req.query.q;
-//   try {
-//     const videos = await video.find({
-//       $or: [
-//         { title: { $regex: searchQuery, $options: 'i' } },
-//         { category: { $regex: searchQuery, $options: 'i' } }
-//       ]
-//     }).populate('user','channelName profilePic userName createdAt');
-//     res.json({ videos });
-//   } catch (error) {
-//     res.status(500).json({ message: 'Error searching videos', error });
-//   }
-// });
-
 app.use('/auth', userRoutes)
 app.use('/api', videoRoutes)
 app.use('/api',searchRoutes)
